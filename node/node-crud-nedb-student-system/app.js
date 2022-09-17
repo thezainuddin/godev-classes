6ce1fd96933b43c23751b41f41ce73229f268e8f
Zain Uddin <a00279673@student.ait.ie>
2022-09-17 18:38:03 +0100

    test endpoint fixed, also added test case

--- node/node-crud-nedb-student-system/app.js
+++ node/node-crud-nedb-student-system/app.js
@@ -6,7 +6,7 @@ app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({extended: true}));
 
 app.get('/api/v1/test', (req, res) => {
-    res.send("Node CRUD Student System is Running")
+    res.status(200).json( { message: "Node CRUD Student System is Running"})
 })
 
 // Import student route
